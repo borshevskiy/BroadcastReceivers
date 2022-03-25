847544bf9cfd7aff7d60b0c70ce6821d69df4aa4
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-25 15:02:08 +0600

    Создание собственного ресивера

--- app/src/main/java/com/borshevskiy/broadcastreceivers/MainActivity.kt
+++ app/src/main/java/com/borshevskiy/broadcastreceivers/MainActivity.kt
@@ -4,17 +4,26 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.borshevskiy.broadcastreceivers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val receiver = MyReceiver()
+
+    private var click = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            sendBroadcast(Intent(MyReceiver.ACTION_CLICKED).putExtra("Click",++click))
+        }
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(MyReceiver.ACTION_CLICKED)
         }
         registerReceiver(receiver, intentFilter)
     }
--- app/src/main/java/com/borshevskiy/broadcastreceivers/MyReceiver.kt
+++ app/src/main/java/com/borshevskiy/broadcastreceivers/MyReceiver.kt
@@ -9,6 +9,10 @@ class MyReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action) {
+            ACTION_CLICKED -> {
+                val click = intent.getIntExtra("Click", 0)
+                Toast.makeText(context, "Clicked $click", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(context, "Airplane mode changed. Turned on: $turnedOn", Toast.LENGTH_SHORT).show()
@@ -17,5 +21,9 @@ class MyReceiver: BroadcastReceiver() {
                 Toast.makeText(context, "Put your smartphone to A/C", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
     }
 }
